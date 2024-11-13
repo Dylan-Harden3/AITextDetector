f195be7a1c49cb669950cf1edb1b80b759858b84
Dylan Harden <dylanharden33@gmail.com>
2024-11-13 15:38:11 -0600

    created datasets, format/refactor data gen/utils

--- generate_data.py
+++ generate_data.py
@@ -1,53 +1,60 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import json
 import torch
 import argparse
 from dotenv import load_dotenv
 import os
-import numpy as np
+from utils import load_model
+
 
 def generate_data(dataset, model, args):
     dataset = load_dataset(
-        dataset,
-        split="train",
-        cache_dir=args.cache,
-        trust_remote_code=True
+        dataset, split="train", cache_dir=args.cache, trust_remote_code=True
     )["document"]
-    
+
     # remove newlines and strip whitespace
     dataset = [x.strip() for x in dataset]
-    dataset = [' '.join(x.split()) for x in dataset]
-    
-    dataset = dataset[:args.n_samples]
-    
+    dataset = [" ".join(x.split()) for x in dataset]
+
+    # only keep samples > 250 words
+    dataset = [x for x in dataset if len(x.split()) > 250]
+
+    dataset = dataset[: args.n_samples]
+
     device = "cuda" if torch.cuda.is_available() else "cpu"
     token = os.getenv("HF_TOKEN")
-    
-    tokenizer = AutoTokenizer.from_pretrained(model, padding_side="right", cache_dir=args.cache, token=token)
-    model = AutoModelForCausalLM.from_pretrained(model, cache_dir=args.cache, token=token).to(device)
+
+    model, tokenizer = load_model(model, args.cache, token)
+
     model.eval()
 
     sampling_args = {
-            "top_p": args.top_p,
-            "temperature": args.temperature,
-            "top_k": args.top_k
-            }
-
-    if tokenizer.pad_token_id is None:
-        tokenizer.pad_token_id = tokenizer.eos_token_id
+        "top_p": args.top_p,
+        "temperature": args.temperature,
+        "top_k": args.top_k,
+    }
 
     data = []
 
     for batch in range(len(dataset) // args.batch_size):
         print(f"Batch {batch} of {len(dataset) // args.batch_size}")
-        human_text = dataset[batch*args.batch_size:(batch+1)*args.batch_size]
+        human_text = dataset[batch * args.batch_size : (batch + 1) * args.batch_size]
 
-        tokenized = tokenizer(human_text, return_tensors="pt", padding=True, return_token_type_ids=False).to(device)
+        tokenized = tokenizer(
+            human_text, return_tensors="pt", padding=True, return_token_type_ids=False
+        ).to(device)
         # use the first 30 tokens to generate an article
         tokenized = {k: v[:, :30] for k, v in tokenized.items()}
+        model.eval()
 
-        outputs = model.generate(**tokenized, max_length=200, do_sample=True, **sampling_args, pad_token_id=tokenizer.eos_token_id, eos_token_id=tokenizer.eos_token_id)
+        outputs = model.generate(
+            **tokenized,
+            max_length=200,
+            do_sample=True,
+            **sampling_args,
+            pad_token_id=tokenizer.eos_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
 
         ai_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
@@ -56,7 +63,11 @@ def generate_data(dataset, model, args):
             human, ai = human[:min_len], ai[:min_len]
             data.append((human, ai))
 
-    output_file = args.output_file if args.output_file else f"{args.model[args.model.index('/'):]}_{args.dataset}.json"
+    output_file = (
+        args.output_file
+        if args.output_file
+        else f"{args.model[args.model.index('/'):]}_{args.dataset}.json"
+    )
 
     with open(output_file, "w") as f:
         json.dump(data, f, indent=4)
@@ -71,10 +82,10 @@ if __name__ == "__main__":
     parser.add_argument("--n_samples", type=int, default=500)
     parser.add_argument("--output_file", type=str)
     parser.add_argument("--cache", type=str, default=".cache")
-    parser.add_argument('--top_k', type=int, default=50)
-    parser.add_argument('--top_p', type=float, default=0.9)
-    parser.add_argument('--temperature', type=float, default=0.8)
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument("--top_k", type=int, default=40)
+    parser.add_argument("--top_p", type=float, default=0.96)
+    parser.add_argument("--temperature", type=float, default=0.8)
+    parser.add_argument("--batch_size", type=int, default=16)
     args = parser.parse_args()
-    
+
     generate_data(args.dataset, args.model, args)
--- utils.py
+++ utils.py
@@ -1,44 +1,59 @@
 import torch
 import torch.nn.functional as F
-from sklearn.metrics import roc_curve, precision_recall_curve, auc
-import matplotlib.pyplot as plt
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from sklearn.metrics import roc_curve, auc
+from transformers import AutoTokenizer, AutoModelForCausalLM, GemmaTokenizerFast
+
 
 def log_likelihood(token_ids, logits):
     logits = logits.view(-1, logits.shape[-1])
     token_ids = token_ids.view(-1)
 
     log_probs = F.log_softmax(logits, dim=-1)
-    actual_token_probs = log_probs.gather(dim=-1, index=token_ids.unsqueeze(-1)).squeeze(-1)
+    actual_token_probs = log_probs.gather(
+        dim=-1, index=token_ids.unsqueeze(-1)
+    ).squeeze(-1)
     return actual_token_probs.mean().item()
 
+
 def rank(token_ids, logits):
-    sorted_indices = torch.argsort(logits, dim=-1, descending=True)
-    
-    matches = (sorted_indices == token_ids.unsqueeze(-1)).nonzero()
+    matches = (logits.argsort(-1, descending=True) == token_ids.unsqueeze(-1)).nonzero()
     ranks = matches[:, -1]
     ranks = ranks.float() + 1
 
     return -ranks.mean().item()
 
-def log_rank(token_ids, logits):
-    sorted_indices = torch.argsort(logits, dim=-1, descending=True)
 
-    matches = (sorted_indices == token_ids.unsqueeze(-1)).nonzero()
+def log_rank(token_ids, logits):
+    matches = (logits.argsort(-1, descending=True) == token_ids.unsqueeze(-1)).nonzero()
     ranks = matches[:, -1]
 
     log_ranks = torch.log(ranks.float() + 1)
 
     return -log_ranks.mean().item()
 
+
 def load_model(model_id, cache=None, token=None):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    tokenizer = AutoTokenizer.from_pretrained(model_id, cache_dir=cache, token=token)
-    model = AutoModelForCausalLM.from_pretrained(model_id, cache_dir=cache, token=token).to(device)
+    # for some reason gemma toknizer was throwing an error, not sure why but this fixed it
+    if model_id != "google/gemma-2-9b":
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_id, {"padding_side": "right"}, cache_dir=cache, token=token
+        )
+    else:
+        tokenizer = GemmaTokenizerFast(vocab_file="/scratch/user/dylanharden3/AITextDetector/dataset/models--google--gemma-2-9b/snapshots/33c193028431c2fde6c6e51f29e6f17b60cbfac6/tokenizer.model", tokenizer_file="/scratch/user/dylanharden3/AITextDetector/dataset/models--google--gemma-2-9b/snapshots/33c193028431c2fde6c6e51f29e6f17b60cbfac6/tokenizer.json")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id, cache_dir=cache, token=token
+    ).to(device)
+
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
 
     return model, tokenizer
 
+
 def roc(human_preds, ai_preds):
-    fpr, tpr, _ = roc_curve([0] * len(human_preds) + [1] * len(ai_preds), human_preds + ai_preds)
+    fpr, tpr, _ = roc_curve(
+        [0] * len(human_preds) + [1] * len(ai_preds), human_preds + ai_preds
+    )
     auc_roc = auc(fpr, tpr)
     return fpr.tolist(), tpr.tolist(), float(auc_roc)
